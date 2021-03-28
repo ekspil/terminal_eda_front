b000d16c3e182a5fbb7282e8400b70e7b499daf2
Alexey Efremov <ekspil@yandex.ru>
2021-03-28 17:07:37 +1000

    kassa

--- src/store/order.js
+++ src/store/order.js
@@ -26,7 +26,7 @@ export default {
     async sendStatus({ dispatch, commit }, data) {
       try {
         const result = await axios.post("http://"+host+":3000/api/terminal/order/sendStatus", data);
-        console.log()
+        console.log(result.data)
         return result.data
       } catch (e) {
         console.log(dispatch, commit);
