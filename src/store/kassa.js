29e615c4d8d500dc52068372e9965d898cd57714
Alexey Efremov <ekspil@yandex.ru>
2021-04-09 11:51:46 +1000

    kassa

--- src/store/kassa.js
+++ src/store/kassa.js
@@ -92,6 +92,7 @@ export default {
           "http://" + host + ":3000/api/kassa/returnChekPayment/",
           body
         );
+        console.log(result.data)
         return result.data;
       } catch (e) {
         console.log(dispatch, commit);
